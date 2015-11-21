d51b1d58f02dfb3e3db18d88480b642e9ba7d816
stone-kee <kb2008@live.cn>
2015-11-21 23:12:18 +0800

    Creat
    Signed-off-by: stone-kee <kb2008@live.cn>

---
+++ Picture/PictureYS/PictureYS/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace PictureYS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picBox = new System.Windows.Forms.PictureBox();
+            this.but_Open = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBox
+            // 
+            this.picBox.Location = new System.Drawing.Point(21, 24);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(351, 303);
+            this.picBox.TabIndex = 0;
+            this.picBox.TabStop = false;
+            // 
+            // but_Open
+            // 
+            this.but_Open.Location = new System.Drawing.Point(297, 370);
+            this.but_Open.Name = "but_Open";
+            this.but_Open.Size = new System.Drawing.Size(75, 23);
+            this.but_Open.TabIndex = 1;
+            this.but_Open.Text = "打开图片";
+            this.but_Open.UseVisualStyleBackColor = true;
+            this.but_Open.Click += new System.EventHandler(this.but_Open_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 424);
+            this.Controls.Add(this.but_Open);
+            this.Controls.Add(this.picBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBox;
+        private System.Windows.Forms.Button but_Open;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Picture/PictureYS/PictureYS/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PictureYS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public Bitmap MyImage;
+
+        private void but_Open_Click(object sender, EventArgs e)
+        {
+            //openFileDialog1.Filter=
+            if(openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                string imageFile=openFileDialog1.FileName;
+                try
+                {
+                        picBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                        MyImage = new Bitmap(imageFile);
+                        picBox.Image=(Image)MyImage;
+                }
+                catch{}
+
+            }
+        }
+    }
+}
