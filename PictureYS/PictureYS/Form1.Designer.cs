cab22ee899b8d995b521d5df8d9c6504428a34a8
stone-kee <kb2008@live.cn>
2015-11-22 12:15:28 +0800

    update1122
    Signed-off-by: stone-kee <kb2008@live.cn>

--- PictureYS/PictureYS/Form1.Designer.cs
+++ PictureYS/PictureYS/Form1.Designer.cs
@@ -33,7 +33,11 @@
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.picBoxNew = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxNew)).BeginInit();
             this.SuspendLayout();
             // 
             // picBox
@@ -62,7 +66,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(515, 34);
+            this.label1.Location = new System.Drawing.Point(19, 373);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(41, 12);
             this.label1.TabIndex = 2;
@@ -71,17 +75,48 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(515, 59);
+            this.label2.Location = new System.Drawing.Point(103, 373);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(41, 12);
             this.label2.TabIndex = 3;
             this.label2.Text = "label2";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(531, 373);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "锐化";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // picBoxNew
+            // 
+            this.picBoxNew.Location = new System.Drawing.Point(531, 24);
+            this.picBoxNew.Name = "picBoxNew";
+            this.picBoxNew.Size = new System.Drawing.Size(429, 314);
+            this.picBoxNew.TabIndex = 5;
+            this.picBoxNew.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(642, 373);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "灰度图";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(666, 424);
+            this.ClientSize = new System.Drawing.Size(1015, 424);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.picBoxNew);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.but_Open);
@@ -89,6 +124,7 @@
             this.Name = "Form1";
             this.Text = "ImageCompress";
             ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxNew)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -101,6 +137,9 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox picBoxNew;
+        private System.Windows.Forms.Button button2;
     }
 }
 
---
+++ PictureYS/PictureYS/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PictureYS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public Bitmap MyImage;
+        public Graphics gImage;
+        //获取图像单个像素数据
+        Color pixel;
+
+        private void but_Open_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Image Files(*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF|All files (*.*)|*.*";
+            if(openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                string imageFile=openFileDialog1.FileName;
+                try
+                {
+                        //显示图像
+                        picBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                        MyImage = new Bitmap(imageFile);
+                        picBox.Image=(Image)MyImage;                    
+
+                }
+                catch{}
+
+            }
+        }
+
+        private void picBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            label1.Text = e.Location.ToString();
+            //获取图像单个像素数据
+            try
+            {               
+                pixel = MyImage.GetPixel(e.Location.X, e.Location.Y);
+                label2.Text = pixel.ToString();
+            }
+            catch { }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //以锐化效果显示图像
+            try
+            {
+                int Height = this.picBox.Image.Height;
+                int Width = this.picBox.Image.Width;
+                Bitmap newBitmap = new Bitmap(Width, Height);
+                Bitmap oldBitmap = (Bitmap)this.picBox.Image;
+                Color pixel;
+                //拉普拉斯模板
+                int[] Laplacian ={ -1, -1, -1, -1, 9, -1, -1, -1, -1 };
+                for (int x = 1; x < Width - 1; x++)
+                    for (int y = 1; y < Height - 1; y++)
+                    {
+                        int r = 0, g = 0, b = 0;
+                        int Index = 0;
+                        for (int col = -1; col <= 1; col++)
+                            for (int row = -1; row <= 1; row++)
+                            {
+                                pixel = oldBitmap.GetPixel(x + row, y + col); 
+                                r += pixel.R * Laplacian[Index];
+                                g += pixel.G * Laplacian[Index];
+                                b += pixel.B * Laplacian[Index];
+                                Index++;
+                            }
+                        //处理颜色值溢出
+                        r = r > 255 ? 255 : r;
+                        r = r < 0 ? 0 : r;
+                        g = g > 255 ? 255 : g;
+                        g = g < 0 ? 0 : g;
+                        b = b > 255 ? 255 : b;
+                        b = b < 0 ? 0 : b;
+                        newBitmap.SetPixel(x - 1, y - 1, Color.FromArgb(r, g, b));
+                    }
+                this.picBoxNew.Image = newBitmap;
+                this.picBoxNew.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "信息提示");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //灰度图
+            try
+            {
+                int Height = this.picBox.Image.Height;
+                int Width = this.picBox.Image.Width;
+                Bitmap newBitmap = new Bitmap(Width, Height);
+                Bitmap oldBitmap = (Bitmap)this.picBox.Image;
+                Color pixel;               
+                for (int x = 1; x < Width - 1; x++)
+                    for (int y = 1; y < Height - 1; y++)
+                    {
+                        int Gray;
+                        pixel = oldBitmap.GetPixel(x, y);
+                        Gray = (pixel.R * 30 + pixel.G * 59 + pixel.B * 11) / 100;
+                        newBitmap.SetPixel(x, y, Color.FromArgb(Gray, Gray, Gray));
+                    }
+                this.picBoxNew.Image = newBitmap;
+                this.picBoxNew.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "信息提示");
+            }
+        }        
+    }
+}
